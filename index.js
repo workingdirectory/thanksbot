658b4c38c4e293918ca328bf77017a3081425ab3
Katie Broida <kbroida@gmail.com>
2020-03-20 17:00:08 -0400

    Add auth response endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const CronJob = require('cron').CronJob;
+const request = require('request');
 const slackInteractions = require('./bot/slackInteractions');
 const slackEvents = require('./bot/slackEvents');
 const { tyReminder } = require('./bot/slackWeb');
@@ -19,8 +20,27 @@ const reminderMessage = new CronJob(
 
 app.use('/slack/events', slackEvents.requestListener());
 app.use('/slack/interactions', slackInteractions.requestListener());
-app.use('/slack/auth', (req, res) => {
-    console.log('req', req);
+app.get('/slack/auth', (req, res) => {
+    const { code } = req.query;
+
+    const options = {
+        uri: 'https://slack.com/api/oauth.access?code='
+            +code+
+            '&client_id='+process.env.SLACK_CLIENT_ID+
+            '&client_secret='+process.env.SLACK_CLIENT_SECRET+
+            '&redirect_uri='+process.env.SLACK_REDIRECT_URI,
+        method: 'GET'
+    }
+    request(options, (error, response, body) => {
+        var JSONresponse = JSON.parse(body)
+        if (!JSONresponse.ok){
+            console.log(JSONresponse)
+            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+        }else{
+            console.log('JSONresponse', JSONresponse)
+            res.send("Success!")
+        }
+    })
 });
 
 reminderMessage.start();
